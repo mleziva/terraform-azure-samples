58e970e30570a74acade279f57007ecd51337fb8
Austin Mleziva <ausmle@microsoft.com>
2020-02-14 09:54:55 -0600

    adding webapps adding variables

---
+++ tf/samples/terraform-azure-fundamentals/terraform.tfvars
@@ -0,0 +1,5 @@
+loc = "centralus"
+tags = {
+  source = "citadel"
+  env    = "training"
+}
--- tf/samples/terraform-azure-fundamentals/variables.tf
+++ tf/samples/terraform-azure-fundamentals/variables.tf
@@ -1,15 +1,21 @@
 variable "loc" {
   description = "Default Azure region"
-  default     = "Central US"
+  default     = "centralus"
 }
 
 variable "rgprefix" {
   description = "Resource group prefix"
   default     = "citadel-training"
 }
+
 variable "tags" {
   default = {
     source = "citadel"
     env    = "training"
   }
+}
+
+variable "webapplocs" {
+  description = "Web app locations"
+  default     = ["northcentralus", "southcentralus", "centralus"]
 }
